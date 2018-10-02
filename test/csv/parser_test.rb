174bce115460b59761f8623f8f83dbd6c47451ae
284km <k.furuhashi10@gmail.com>
2018-10-02 20:11:13 +0900

    :green_heart: Use test-unit

--- test/csv/parser_test.rb
+++ test/csv/parser_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class Csv::ParserTest < Minitest::Test
+class Csv::ParserTest < Test::Unit::TestCase
   def test_that_it_has_a_version_number
     refute_nil ::Csv::Parser::VERSION
   end
